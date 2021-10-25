34d5fc9aeebfb135f2ab73b0ff4321ec32895023
firamuongxa <firaxaxa@gmail.com>
2021-10-25 11:30:33 +0700

    Long:Admin login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,9 @@
-@include('head')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('head')
+</head>
 
 <body id="default_theme" class="home_page1">
     <!-- header -->
@@ -27,7 +32,7 @@
                     <div class="col-md-6">
                         <div class="float-right">
                             <ul class="top_links">
-                                <li><a href="#"><img src="/frontend/images/profile_icon.png" alt="#" /></a></li>
+                                <li><a href="{{ URL::to('/login') }}"><img src="/frontend/images/profile_icon.png" alt="#" /></a></li>
                                 <li class="searchbar">
                                     <input class="search_input" type="text" name="" placeholder="Search...">
                                     <a href="#" class="search_icon"><img src="/frontend/images/search_icon.png" alt="#" /></a>
