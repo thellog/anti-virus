982a18b1e543a9f8d93a61645324346fb911e676
firamuongxa <firaxaxa@gmail.com>
2021-11-05 10:48:07 +0700

    adjust section

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -2,82 +2,83 @@
 <html lang="en">
 
 <head>
-    @include('admin.head')
+  @include('admin.head')
 </head>
 
 <body class="hold-transition sidebar-mini">
-    <div class="wrapper">
-        <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-            <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                </li>
-            </ul>
+  <div class="wrapper">
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+      </ul>
 
-            <!-- Right navbar links -->
-            <ul class="navbar-nav ml-auto">
+      <!-- Right navbar links -->
+      <ul class="navbar-nav ml-auto">
 
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                        <i class="fas fa-expand-arrows-alt"></i>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <!-- /.navbar -->
-
-
-        @include('admin.sidebar')
+        <li class="nav-item">
+          <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+            <i class="fas fa-expand-arrows-alt"></i>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.navbar -->
 
 
-        <div class="content-wrapper">
-
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-
-                    @include('alert')
-
-                    <div class="row">
-                        <!-- left column -->
-                        <div class="col-md-12">
-                            <!-- jquery validation -->
-                            <div class="card card-primary mt-3">
-                                <div class="card-header">
-                                    <h3 class="card-title">{{ $title }}</h3>
-                                </div>
-
-                                @yield('content')
-
-                            </div>
-                            <!-- /.card -->
-                        </div>
-                        <!--/.col (left) -->
-                        <!-- right column -->
-                        <div class="col-md-6">
-
-                        </div>
-                        <!--/.col (right) -->
-                    </div>
-                    <!-- /.row -->
-                </div><!-- /.container-fluid -->
-            </section>
-            <!-- /.content -->
-        </div>
+    @include('admin.sidebar')
 
 
-        <!-- /.content-wrapper -->
-        <footer class="main-footer">
-            <div class="float-right d-none d-sm-block">
-                <b>Version</b> 3.1.0
+    <div class="content-wrapper">
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+
+          @include('alert')
+
+          <div class="row">
+            <!-- left column -->
+            <div class="col-md-12">
+              <!-- jquery validation -->
+              <div class="card card-primary mt-3">
+                <div class="card-header">
+                  <h3 class="card-title">{{ $title }}</h3>
+                </div>
+
+                @yield('chart-1')
+
+                @yield('chart-2')
+              </div>
+              <!-- /.card -->
             </div>
-            <strong>Copyright &copy; 2014-2021.</strong> All rights reserved.
-        </footer>
+            <!--/.col (left) -->
+            <!-- right column -->
+            <div class="col-md-6">
+
+            </div>
+            <!--/.col (right) -->
+          </div>
+          <!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
     </div>
-    <!-- ./wrapper -->
-    @include('admin.footer')
+
+
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+      <div class="float-right d-none d-sm-block">
+        <b>Version</b> 3.1.0
+      </div>
+      <strong>Copyright &copy; 2014-2021.</strong> All rights reserved.
+    </footer>
+  </div>
+  <!-- ./wrapper -->
+  @include('admin.footer')
 </body>
 
 </html>
